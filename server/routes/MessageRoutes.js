dd04e68c4be9d8c41073dcb380df703b45d712d6
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-10-31 22:06:19 +0530

    Implemented Sidebar Contacts Page

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addAudioMesage, addImageMesage, addMessage, getMessage } from "../controllers/MessageController.js";
+import { addAudioMesage, addImageMesage, addMessage, getInitialContactSwitchMessages, getMessage } from "../controllers/MessageController.js";
 import multer from "multer";
  
 const router = Router();
@@ -11,5 +11,6 @@ router.post("/add-message",addMessage);
 router.get("/get-messages/:from/:to",getMessage);
 router.post("/add-image-message",uploadImage.single("image"), addImageMesage);
 router.post("/add-audio-message",upload.single("audio"), addAudioMesage);
+router.get("/get/initial/contacts/:from",getInitialContactSwitchMessages)
 
 export default router;
